a72558d7d6d8857015749410c54485efa947ab85
Reyhan Rasyid Ziddan <zreryhanrasyid@gmail.com>
2023-09-08 14:41:03 +0700

    first commit

---
+++ reyhan-rz-react-test/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ViewList from './Components/ViewList';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>HELLO</h1>
+      <ViewList />
+    </div>
+  );
+}
+
+export default App;
